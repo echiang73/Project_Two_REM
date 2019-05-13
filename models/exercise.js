360257a07926127c60b92a653cbdb84c1c01435a
Ryan Grady <ryangrady250@gmail.com>
2019-05-13 01:16:03 -0700

    completed preliminary sequelize code

--- models/exercise.js
+++ models/exercise.js
@@ -1,23 +1,19 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Example = sequelize.define("Example", {
-//     text: DataTypes.STRING,
-//     description: DataTypes.TEXT
-//   });
-//   return Example;
-// };
+
 module.exports = function(sequelize, DataTypes) {
   var Exercise = sequelize.define("Exercise", {
-    id: {
-      DataType: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+  
       workout_type: DataTypes.STRING,
       exercise_name: DataTypes.STRING,
-      exer_img_url: DataTypes.INTEGER,
+      exer_img_url: DataTypes.STRING,
       weight: DataTypes.INTEGER,
       repetitions: DataTypes.STRING,
       sets: DataTypes.INTEGER
   });
+
+  Exercise.associate = function(models) {
+    // Associating exercise  with user history
+    
+    Exercise.hasMany(models.History);
+  };
   return Exercise;
 };
--- models/history.js
+++ models/history.js
@@ -1,28 +1,30 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Example = sequelize.define("Example", {
-//     text: DataTypes.STRING,
-//     description: DataTypes.TEXT
-//   });
-//   return Example;
-// };
+
 module.exports = function(sequelize, DataTypes) {
     var History = sequelize.define("History", {
-      id: {
-        DataType: DataType.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-        user_id: DataTypes.INTEGER,
-        date_completed: DataTypes.DATE,
-        exercise_id: DataTypes.STRING,
-        weight: DataTypes.INTEGER,
-        repetitions: DataTypes.STRING,
-        sets_1: DataTypes.BOOLEAN,
-        sets_2: DataTypes.BOOLEAN,
-        sets_3: DataTypes.BOOLEAN,
-        sets_4: DataTypes.BOOLEAN,
-        sets_5: DataTypes.BOOLEAN
+   
+//History will be plurilized by sequelize.. Maybe?    
+
+    user_id: DataTypes.INTEGER,
+    date_completed: DataTypes.DATE,
+    exercise_id: DataTypes.STRING,
+    weight: DataTypes.INTEGER,
+    repetitions: DataTypes.STRING,
+    sets_1: DataTypes.BOOLEAN,
+    sets_2: DataTypes.BOOLEAN,
+    sets_3: DataTypes.BOOLEAN,
+    sets_4: DataTypes.BOOLEAN,
+    sets_5: DataTypes.BOOLEAN
     });
+    History.associate = function(models) {
+      // We're saying that a user history should belong to an exercise
+      // A workout history can't be created without an exercise due to the foreign key constraint
+      History.belongsTo(models.Exercise, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+
     return History;
   };
   
