97e2cb612897e1f91e58a9ea356236e3aa2bcd71
Ryan Grady <ryangrady250@gmail.com>
2019-05-15 22:36:48 -0700

    merged software builds

--- models/exercise.js
+++ models/exercise.js
@@ -1,26 +1,24 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Example = sequelize.define("Example", {
-//     text: DataTypes.STRING,
-//     description: DataTypes.TEXT
-//   });
-//   return Example;
-// };
+
+
+
+
 module.exports = function(sequelize, DataTypes) {
   var Exercise_template = sequelize.define("Exercise_template", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+  
       workout_type: DataTypes.STRING,
       exercise_name: DataTypes.STRING,
-      exer_img_url1: DataTypes.STRING,
-      exer_img_url2: DataTypes.STRING,
+      exer_img_url: DataTypes.STRING,
       weight: DataTypes.INTEGER,
       repetitions: DataTypes.STRING,
       sets: DataTypes.INTEGER
-  },{
-    freezeTableName: true
   });
+
+  Exercise_template.associate = function(models) {
+    // Associating exercise  with user history
+    
+    Exercise_template.hasMany(models.User_history);
+  };
   return Exercise_template;
 };
+
+
--- models/history.js
+++ models/history.js
@@ -1,30 +1,28 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Example = sequelize.define("Example", {
-//     text: DataTypes.STRING,
-//     description: DataTypes.TEXT
-//   });
-//   return Example;
-// };
-module.exports = function(sequelize, DataTypes) {
+
+
+  module.exports = function(sequelize, DataTypes) {
     var User_history = sequelize.define("User_history", {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-        user_id: DataTypes.INTEGER,
-        date_completed: DataTypes.DATE,
-        exercise_id: DataTypes.STRING,
-        weight: DataTypes.INTEGER,
-        repetitions: DataTypes.STRING,
-        sets_1: DataTypes.BOOLEAN,
-        sets_2: DataTypes.BOOLEAN,
-        sets_3: DataTypes.BOOLEAN,
-        sets_4: DataTypes.BOOLEAN,
-        sets_5: DataTypes.BOOLEAN
-    },{
-      freezeTableName: true
+   
+  //History will be plurilized by sequelize     
+  
+    user_id: DataTypes.INTEGER,
+    date_completed: DataTypes.DATE,
+    sets_1: DataTypes.BOOLEAN,
+    sets_2: DataTypes.BOOLEAN,
+    sets_3: DataTypes.BOOLEAN,
+    sets_4: DataTypes.BOOLEAN,
+    sets_5: DataTypes.BOOLEAN
     });
+    User_history.associate = function(models) {
+      // We're saying that a user history should belong to an exercise
+      // A workout history can't be created without an exercise due to the foreign key constraint
+      User_history.belongsTo(models.Exercise_template, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
     return User_history;
   };
   
