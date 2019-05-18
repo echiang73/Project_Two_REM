eb74b87f0eaf21cc3541556ea85c136606a455c1
Neo <echiang73@gmail.com>
2019-05-18 11:51:40 -0700

    Final version with corrected progress.js file

--- public/js/progress.js
+++ public/js/progress.js
@@ -10,6 +10,10 @@ $("#nav-analytics-tab").on("click", function(){
             var setNumber = 0;
             var xAxisData = [];
             var yAxisData = [];
+            var yAxisDataUpper = [];
+            var yAxisDataLower = [];
+            var yAxisDataCardio = [];
+            
 
 
             // console.log( res );
@@ -63,34 +67,83 @@ $("#nav-analytics-tab").on("click", function(){
 
             console.log(xAxisData);
             console.log(yAxisData);
+            console.log(res);
+            
+            console.log(res[0].Exercise_template.workout_type);
+
+            //Parse out y axis data into upper body, lower body, and cardio
+
+            for (var i=0;i<xAxisData.length;i++){
+
+                if (res[i].Exercise_template.workout_type == "Upper Body")
+                { 
+                    yAxisDataUpper[i]=yAxisData[i];
+                    yAxisDataLower[i]= 0;
+                    yAxisDataCardio[i]=0;
+                   
+                }
+                if (res[i].Exercise_template.workout_type == "Lower Body")
+                { 
+                    yAxisDataUpper[i]=0;
+                    yAxisDataLower[i]=yAxisData[i];
+                    yAxisDataCardio[i]=0;
+
+                }
+                if (res[i].Exercise_template.workout_type == "Core/Cardio")
+                { 
+                    yAxisDataUpper[i]=0;
+                    yAxisDataLower[i]=0;
+                    yAxisDataCardio[i]=yAxisData[i];
+
+                }
 
 
+            }
 
-
+            console.log(yAxisDataUpper);
+            console.log(yAxisDataLower);
+            console.log(yAxisDataCardio);
             //Create a new object
             var userProgressChart = new Chart("myChart", {
                 type: "bar", //bar, horizontalBar, pie, line, doughnut, radar, polarArea
                 data: {
                     labels: xAxisData,
                     datasets: [{
-                        label: "Sets",
-                        data: yAxisData,
+                        label: "Upper Body",
+                        data: yAxisDataUpper,
+                        
 
                         backgroundColor:[
 
                             'rgba(255, 99, 132, 0.6)',
-                
-                            'rgba(54, 162, 235, 0.6)',
-                
-                            'rgba(255, 206, 86, 0.6)',
-                
-                            'rgba(75, 192, 192, 0.6)',
-                
-                            'rgba(153, 102, 255, 0.6)',
-                
-                            'rgba(255, 159, 64, 0.6)',
-                
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(255, 99, 132, 0.6)',
                             'rgba(255, 99, 132, 0.6)'
+
+                
+                            // 'rgba(54, 162, 235, 0.6)',
+                
+                            // 'rgba(255, 206, 86, 0.6)',
+                
+                            // 'rgba(75, 192, 192, 0.6)',
+                
+                            // 'rgba(153, 102, 255, 0.6)',
+                
+                            // 'rgba(255, 159, 64, 0.6)',
+                
+                            // 'rgba(255, 99, 132, 0.6)'
                 
                           ],
                 
@@ -103,11 +156,92 @@ $("#nav-analytics-tab").on("click", function(){
                           hoverBorderColor:'#000'
                 
                         
-                    }]
+                    },{
+                        label: "Lower Body",
+                        data: yAxisDataLower,
+                        
+
+                        backgroundColor:[
+
+                
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(54, 162, 235, 0.6)'
+                
+                          ],
+                
+                          borderWidth:1,
+                
+                          borderColor:'#777',
+                
+                          hoverBorderWidth:3,
+                
+                          hoverBorderColor:'#000'
+                
+                        
+                    },{
+                        label: "Core/ Cardio",
+                        data: yAxisDataCardio,
+                        
+
+                        backgroundColor:[
+
+                
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(75, 192, 192, 0.6)'
+                          
+                          ],
+                
+                          borderWidth:1,
+                
+                          borderColor:'#777',
+                
+                          hoverBorderWidth:3,
+                
+                          hoverBorderColor:'#000'
+                
+                        
+                    }],
                 },
                 options: {
                     
+                    legend: {
+                       
+                        display: true,
+                        text: "this is a string",
 
+                        labels: {
+                            fontColor: 'rgb(255, 99, 132)'
+                        }
+                    },
+                       
                     title:{
                         display:true,
                         text:'Workout Progress',
@@ -139,4 +273,3 @@ $("#nav-analytics-tab").on("click", function(){
     });
 
   });
-
